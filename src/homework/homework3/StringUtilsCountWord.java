4b725c0c4c5e2028e8fdf195f8f7f20d94ba210f
psannikov <p.sannikov@volga.rt.ru>
2023-04-21 18:16:06 +0400

    dop11 FIX

--- src/homework/homework3/StringUtilsCountWord.java
+++ src/homework/homework3/StringUtilsCountWord.java
@@ -2,7 +2,17 @@ package homework.homework3;
 
 public class StringUtilsCountWord {
     public static int countWord(String input, String word) {
-        return input.toLowerCase().split(word.toLowerCase()).length;
+        int cnt = 0;
+        boolean cicle = true;
+        while (cicle) {
+            if (input.toLowerCase().equals(input.toLowerCase().replaceFirst(word.toLowerCase(),""))) {
+                cicle = false;
+            } else {
+                input = input.toLowerCase().replaceFirst(word.toLowerCase(),"");
+                cnt++;
+            }
+        }
+        return cnt;
     }
 
     private StringUtilsCountWord() {
@@ -11,6 +21,6 @@ public class StringUtilsCountWord {
 
     public static void main(String[] args) {
         System.out.println("При вводе строки \"Test string for test\" и искомом слове \"test\" метод должен вернуть 2 - "
-                + StringUtilsCountWord.countWord("Test string for teSTTEST", "tEst"));
+                + StringUtilsCountWord.countWord("Test string for teStTest", "rergdg"));
     }
 }
