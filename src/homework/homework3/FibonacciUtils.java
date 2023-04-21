4953c1c0062657df00e84d42cc4e5213b6fc228c
psannikov <p.sannikov@volga.rt.ru>
2023-04-21 22:41:56 +0400

    dop3

--- src/homework/homework3/FibonacciUtils.java
+++ src/homework/homework3/FibonacciUtils.java
@@ -1,11 +1,23 @@
 package homework.homework3;
 
+import java.math.BigInteger;
+
 public class FibonacciUtils {
-    public static int getFibonacciSum(byte limit) {
-        if (limit <= 1) {
-            return limit;
+    public static String getFibonacciSum(byte limit) {
+        BigInteger res = new BigInteger("0");
+        if (limit > 1) {
+            res = res.add(new BigInteger("1"));
+            BigInteger n0 = new BigInteger("0");
+            BigInteger n1 = new BigInteger("1");
+            BigInteger n2;
+            for (int i = 3; i <= limit; i++) {
+                n2 = n0.add(n1);
+                res = res.add(new BigInteger(String.valueOf(n2)));
+                n0 = n1;
+                n1 = n2;
+            }
         }
-        return getFibonacciSum((byte) (limit - 1)) + getFibonacciSum((byte) (limit - 2));
+        return res.toString();
     }
 
     private FibonacciUtils(){
@@ -13,6 +25,6 @@ public class FibonacciUtils {
     }
 
     public static void main(String[] args) {
-        System.out.println("Сумма чисел фибоначчи до 5 должна равняться 7 - " + FibonacciUtils.getFibonacciSum((byte) 5));
+        System.out.println("Сумма чисел фибоначчи до 5 должна равняться 7 - " + FibonacciUtils.getFibonacciSum((byte) 100));
     }
 }
