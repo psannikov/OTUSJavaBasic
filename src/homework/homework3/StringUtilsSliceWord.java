c99033cc595a6c98b70090761803e55cadc1e6e3
psannikov <p.sannikov@volga.rt.ru>
2023-04-23 23:00:16 +0400

    dop10 FIX

--- src/homework/homework3/StringUtilsSliceWord.java
+++ src/homework/homework3/StringUtilsSliceWord.java
@@ -2,14 +2,14 @@ package homework.homework3;
 
 public class StringUtilsSliceWord {
     public static String getSlice(String input, int leftEdge, int rightEdge) {
-        if ((rightEdge <= leftEdge) ||
+        if ((rightEdge < leftEdge) ||
                 (rightEdge < 0) ||
                 (leftEdge < 0) ||
-                (rightEdge > input.length()) ||
-                (leftEdge > input.length())) {
+                (leftEdge+rightEdge > input.length()) ||
+                (leftEdge >= input.length())) {
             return input;
         }
-        return input.substring(leftEdge,rightEdge);
+        return input.substring(leftEdge,leftEdge+rightEdge);
     }
 
     private StringUtilsSliceWord() {
@@ -18,7 +18,7 @@ public class StringUtilsSliceWord {
 
     public static void main(String[] args) {
         System.out.println("При вводе строки \"Test\" с индексами 1 и 4 метод должен вернуть \"est\" - "
-                + "\"" + StringUtilsSliceWord.getSlice("Test", 2,2) +"\"");
+                + "\"" + StringUtilsSliceWord.getSlice("Test", 1,4) +"\"");
 
     }
 }
