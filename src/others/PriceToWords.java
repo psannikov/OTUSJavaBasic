358d78512d79d3de10e716ab0ff6d77e5ac17e83
psannikov <p.sannikov@volga.rt.ru>
2023-04-25 20:47:31 +0400

    read and write file, check exists, class user

--- src/others/PriceToWords.java
+++ src/others/PriceToWords.java
@@ -2,6 +2,9 @@ package others;
 
 public class PriceToWords {
     public static void main(String[] args) {
-
+        WorkWithUser userData = new WorkWithUser();
+        System.out.println(userData.currency);
+        System.out.println(userData.form);
+        System.out.println(userData.isChanged);
     }
 }
