68343b01cfbd91dff68c40bc696a799703645b2e
psannikov <p.sannikov@volga.rt.ru>
2023-04-29 15:48:33 +0400

    даление из ветки hw4 лишних данных

--- src/final_project/PriceToWords.java
+++
@@ -1,9 +0,0 @@
-package final_project;
-
-public class PriceToWords {
-    public static void main(String[] args) {
-        Currency currency = new Currency();
-        Value value = new Value(currency);
-        System.out.println(value.convertValueToString());
-    }
-}
