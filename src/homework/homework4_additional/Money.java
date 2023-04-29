54a9880a142de5aeed70366cbc3216ce01f00ea4
psannikov <p.sannikov@volga.rt.ru>
2023-04-30 00:11:05 +0400

    Доп class Money

--- src/homework/homework4_additional/Money.java
+++ src/homework/homework4_additional/Money.java
@@ -4,19 +4,42 @@ package homework.homework4_additional;
 Целую и дробную части в строке разделять запятой.*/
 
 public class Money {
-//TODO
+    private int base;
+    private int fractional;
 
     public Money(int base, int fractional) {
-//TODO
+        this.base = base;
+        this.fractional = fractional;
     }
 
     public String getAmount() {
-//TODO
-        return "";
+        String baseString = switch (this.base%10) {
+            case 1 -> switch (this.base%100) {
+                case 11 -> "рублей";
+                default -> "рубль";
+            };
+            case 2,3,4 -> switch (this.base%100) {
+                case 12,13,14 ->"рублей";
+                default -> "рубля";
+            };
+            default -> "рублей";
+        };
+        String fractionalString = switch (this.fractional%10) {
+            case 1 -> switch (this.fractional%100) {
+                case 11 -> "копеек";
+                default -> "копейка";
+            };
+            case 2,3,4 -> switch (this.fractional%100) {
+                case 12, 13, 14 -> "копеек";
+                default -> "копейки";
+            };
+            default -> "копеек";
+        };
+        return this.base + " " + baseString + ", " + this.fractional + " " + fractionalString;
     }
 
     public static void main(String[] args) {
-        System.out.println("100 рублей, 90 копеек: " + new Money(100, 90).getAmount());
-        System.out.println("0 рублей, 1 копейка: " + new Money(0, 1).getAmount());
+        System.out.println("100 рублей, 90 копеек: " + new Money(22, 12).getAmount());
+        System.out.println("0 рублей, 1 копейка: " + new Money(12, 3).getAmount());
     }
 }
