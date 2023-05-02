427b6a2915edfab866198b7cddc3f0ef7171c3c0
psannikov <p.sannikov@volga.rt.ru>
2023-05-02 14:05:21 +0400

    Доп пытался исправить ошибки, локально работает но не принимает

---
+++ src/homework/homework4_additional/Test.java
@@ -0,0 +1,9 @@
+package homework.homework4_additional;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(String.class);
+
+        System.out.println("я строка".getClass());
+    }
+}
