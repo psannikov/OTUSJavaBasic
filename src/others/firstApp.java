a484793c8920f1b824337513dec3846976273ae6
psannikov <p.sannikov@volga.rt.ru>
2023-05-14 23:38:14 +0400

    восстанавливаю main

---
+++ src/others/firstApp.java
@@ -0,0 +1,8 @@
+package others;
+
+public class firstApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("HW2 home pc");
+    }
+}
