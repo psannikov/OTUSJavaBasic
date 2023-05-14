e5bc81b119d034e125a55b61ffb5c6a4bf1da755
psannikov <p.sannikov@volga.rt.ru>
2023-05-15 00:01:48 +0400

    дз6 создал классы

--- src/homework/homework6/Client.java
+++ src/homework/homework6/Client.java
@@ -1,4 +1,20 @@
 package homework.homework6;
 
+import java.util.Set;
+
 public class Client {
+    private String name;
+    private byte age;
+    private Set<Account> clientAccounts;
+
+    public Set<Account> getClientAccounts () {
+        return clientAccounts;
+    }
+    public void addClientAccount (Account account) {
+        this.clientAccounts.add(account);
+    }
+    public Client(String name, byte age) {
+        this.name = name;
+        this.age = age;
+    }
 }
