25003973eb74a8d6ee071a023dc0bf25d08f06ad
psannikov <p.sannikov@volga.rt.ru>
2023-05-17 08:38:06 +0400

    закончил допики 4. осталось сдать на портал

--- src/homework/additional/ObjectsTaskStringToObject.java
+++ src/homework/additional/ObjectsTaskStringToObject.java
@@ -5,19 +5,22 @@ package homework.additional;
 который принимает строковую переменную - вернуть null*/
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 
 public class ObjectsTaskStringToObject {
     private ObjectsTaskStringToObject(){}
 
-    public static <T> T makeObjectFromString(String input, T anyObject) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-        Class<?>[] parameterTypes = {input.getClass()};
-        Constructor constructor = anyObject.getClass().getConstructor(parameterTypes);
-        return (constructor == null) ? null : (T) constructor.newInstance(input);
+    public static <T> T makeObjectFromString(String input, T anyObject){
+        try {
+            Class<?>[] parameterTypes = {input.getClass()};
+            Constructor<?> constructor = anyObject.getClass().getConstructor(parameterTypes);
+            return (T) constructor.newInstance(input);
+        } catch (Exception e) {
+            return null;
+        }
     }
 
-    public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args){
         System.out.println("Создаем число из строки: " + makeObjectFromString("15", 1));
         System.out.println("Создаем булево из строки: " + makeObjectFromString("true", true));
         System.out.println("Создаем список из строки, должно не получиться: " + makeObjectFromString("1,2,3", new ArrayList<>()));
