6a93c88352bd78a1c5873b75ca5ff225d530ea1d
psannikov <p.sannikov@volga.rt.ru>
2023-05-26 07:22:19 +0400

    снова потерялось чтото

---
+++ src/homework/homework2/ide/HomeWork.java
@@ -0,0 +1,12 @@
+package homework.homework2.ide;
+import java.util.Base64;
+import java.util.Scanner;
+public class HomeWork {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter you full name");
+        String fullName = sc.nextLine();
+        String encodedFullName = Base64.getEncoder().encodeToString(fullName.getBytes());
+        System.out.println("Program result: " + encodedFullName);
+    }
+}
