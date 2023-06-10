0c362ddf02da8d42d904a571e9e3b74b0c8affb8
psannikov <p.sannikov@volga.rt.ru>
2023-06-11 00:05:20 +0400

    первый коммит, копирование исходников

---
+++ src/homework/homework8/Dev.java
@@ -0,0 +1,17 @@
+package homework.homework8;
+
+import java.util.Random;
+
+public class Dev {
+    public static void main(String[] args) {
+        System.out.println(new Random().nextInt());
+        /*Тест 1 бросок больше 6
+        Тест 2 меньше 1
+        Тест 3 броски игроков одинаковы
+        Тест 4 ссылки на игоков одно и тоже
+        ОШибка 1 аут оф рендж броска
+        Ошибка 2 равенство
+         */
+
+    }
+}
