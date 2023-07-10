900f9c9aa9d59f710d1c11509eb0cd5547875919
psannikov <psannikov87@gmail.com>
2023-07-10 23:21:09 +0400

    add

--- src/homework/additional/FunctionUtilsShiftZeroes.java
+++ src/homework/additional/FunctionUtilsShiftZeroes.java
@@ -12,7 +12,7 @@ public class FunctionUtilsShiftZeroes {
     public static void main(String[] args) {
         System.out.println(
                 "При вводе числа 3435, метод должен вернуть 4095 - '"
-                        + FunctionUtilsShiftZeroes.shiftZeroes(3435)
+                        + FunctionUtilsShiftZeroes.shiftZeroes(7)
                         + "'"
         );
     }
